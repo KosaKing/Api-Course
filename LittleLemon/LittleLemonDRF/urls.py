5dedbbea626406242dfb3c7b03a47079ce1e8d9d
KosaKing <Filip.Kosowski@interia.pl>
2023-03-15 21:59:25 +0100

    I added experiment file - following with the django course.
    To be continued - WEEK2 (deserialisation and validation)

---
+++ LittleLemon/LittleLemonDRF/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path 
+from . import views 
+  
+urlpatterns = [ 
+    # path('ratings', views.RatingsView.as_view()), 
+] 
+
+
+# Useful shit 
+# week2 - Generic views and ViewSets in DRF
---
+++ experiment/experimentAPI/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('menu-item', views.MenuItemView.as_view()),
+    path('menu-item/<int:pk>/', views.SpecyficMenuItemView.as_view()),
+]
